b37e81acb85ed1e81e7a623280e38b9e2ec54031
Volodymyr <mospan.volodymyr@gmail.com>
2026-03-27 08:29:39 +0200

    adds more points for zone 1,2 and 3

--- src/utilities/dev.js
+++ src/utilities/dev.js
@@ -2,35 +2,37 @@ import { saveAs } from "file-saver";
 const devBtn = document.getElementById("dev");
 
 const newJson = {
-  type: "Feature",
-  properties: {},
-  style: {
-    color: "#000000",
-    width: 0,
-    opacity: 1,
-    fillColor: "#000000",
-    fillOpacity: 0,
-  },
-  geometry: {
-    type: "Polygon",
-    coordinates: [
-      [
-        ["Y", "X"],
-        ["Y", "X"],
-        ["Y", "X"],
-        ["Y", "X"],
-        ["Y", "X"],
-      ],
-    ],
-    properties: { coordSys: "Local,MCK-32" },
-  },
+  type: "MultiPolygon",
+  coordinates: [[[]]], // coordinates: [[[["Y", "X"]]]],
+  properties: { coordSys: "SC63" },
 };
-
-for (let x = 5560000.0; x < 5610000.0; x += 5000) {
-  for (let y = 280000.0; y < 325000.0; y += 5000) {
-    newJson.geometry.coordinates[0].push([y, x]);
+for (let x = 4975000; x < 5775000; x += 10000) {
+  for (let y = 3180000; y < 3430000; y += 10000) {
+    newJson.coordinates[0][0].push([y, x]);
   }
 }
+// const newJson = {
+//   type: "Feature",
+//   properties: {},
+//   style: {
+//     color: "#000000",
+//     width: 0,
+//     opacity: 1,
+//     fillColor: "#000000",
+//     fillOpacity: 0,
+//   },
+//   geometry: {
+//     type: "Polygon",
+//     coordinates: [[["Y", "X"]]],
+//     properties: { coordSys: "SC63" },
+//   },
+// };
+
+// for (let x = 5275000; x < 5775000; x += 10000) {
+//   for (let y = 2185000; y < 2425000; y += 10000) {
+//     newJson.geometry.coordinates[0].push([y, x]);
+//   }
+// }
 
 devBtn.addEventListener("click", onDevBtn);
 
